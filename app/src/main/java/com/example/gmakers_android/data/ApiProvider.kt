d81d6d8e9d1d09a3919b7f47d0f81221588a40e9
dlswer23 <lbj4298@naver.com>
2021-08-07 18:31:15 +0900

    [UPDATE] sign api

---
+++ app/src/main/java/com/example/gmakers_android/data/ApiProvider.kt
@@ -0,0 +1,4 @@
+package com.example.gmakers_android.data
+
+object ApiProvider {
+}
--- app/src/main/java/com/example/gmakers_android/data/remote/sign/RegisterApi.kt
+++ app/src/main/java/com/example/gmakers_android/data/remote/sign/RegisterApi.kt
@@ -1,4 +1,4 @@
 package com.example.gmakers_android.data.remote.sign
 
-class SignImpl {
+interface RegisterApi {
 }
