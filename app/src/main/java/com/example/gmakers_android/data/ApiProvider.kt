302ab7d2c4d5cd62c2557f92bf173e311e55f37a
dlswer23 <lbj4298@naver.com>
2021-08-16 02:24:03 +0900

    [MODIFY] BASE_URL

--- app/src/main/java/com/example/gmakers_android/data/ApiProvider.kt
+++ app/src/main/java/com/example/gmakers_android/data/ApiProvider.kt
@@ -1,20 +1,15 @@
 package com.example.gmakers_android.data
 
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 
 object ApiProvider {
     private const val BASE_URL: String = "http://52.79.197.237:8080"
     private var retrofit : Retrofit
 
     init {
-        val loggingInterceptor = HttpLoggingInterceptor()
-        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
         val client = OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
             .build()
         retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
@@ -26,7 +21,6 @@ object ApiProvider {
     fun getClient(): Retrofit {
         return retrofit
     }
-
 
 
 
