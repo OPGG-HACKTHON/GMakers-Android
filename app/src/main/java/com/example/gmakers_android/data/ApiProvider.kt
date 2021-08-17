6dd6f0d1dd389b119d6cd34b478a68b990f06b0e
dlswer23 <lbj4298@naver.com>
2021-08-17 23:08:07 +0900

    [MODIFY] api provider

--- app/src/main/java/com/example/gmakers_android/data/ApiProvider.kt
+++ app/src/main/java/com/example/gmakers_android/data/ApiProvider.kt
@@ -1,27 +1,24 @@
 package com.example.gmakers_android.data
 
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiProvider {
-    private const val BASE_URL: String = "http://52.79.197.237:8080"
-    private var retrofit : Retrofit
 
-    init {
-        val client = OkHttpClient.Builder()
-            .build()
-        retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
-            .build()
+    private var instance : Retrofit? = null
+    private val gson = GsonBuilder().setLenient().create()
+
+    fun getInstnace() : Retrofit {
+        if(instance == null){
+            instance = Retrofit.Builder()
+                .baseUrl("http://52.79.197.237:8080")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+        return instance!!
     }
-
-    fun getClient(): Retrofit {
-        return retrofit
-    }
-
 
 
 }
