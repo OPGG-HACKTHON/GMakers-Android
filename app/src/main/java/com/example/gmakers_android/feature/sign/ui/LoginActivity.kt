0fc674bda7cc7b2fba0c4cf51754dc52c2f2905f
dlswer23 <lbj4298@naver.com>
2021-08-17 23:10:11 +0900

    [UPDATE] finish login

--- app/src/main/java/com/example/gmakers_android/feature/sign/ui/LoginActivity.kt
+++ app/src/main/java/com/example/gmakers_android/feature/sign/ui/LoginActivity.kt
@@ -3,25 +3,40 @@ package com.example.gmakers_android.feature.sign.ui
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import com.example.gmakers_android.R
 import com.example.gmakers_android.data.base.BaseActivity
+import com.example.gmakers_android.data.local.SharedPreferenceStorage
 import com.example.gmakers_android.databinding.ActivityLoginBinding
 import com.example.gmakers_android.feature.sign.viewmodel.LoginViewModel
+import com.example.gmakers_android.feature.sign.viewmodel.RegisterViewModel
 
-class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+class LoginActivity: BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
 
-    override val vm: LoginViewModel by viewModels()
+    override val vm: LoginViewModel = LoginViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.lifecycleOwner = this
+        binding.vm = vm
+        goRegister()
+
     }
 
     override fun onStart() {
         super.onStart()
-        goRegister()
+        successLogin()
 
+
+    }
+
+    fun successLogin(){
+        vm.run {
+            toastMessage.observe(this@LoginActivity, { message ->
+                Toast.makeText(this@LoginActivity, message, Toast.LENGTH_SHORT).show()
+            })
+        }
     }
 
 
--- app/src/main/java/com/example/gmakers_android/feature/sign/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/gmakers_android/feature/sign/viewmodel/LoginViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.gmakers_android.feature.sign.viewmodel
-import android.util.Log
+
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.gmakers_android.data.ApiProvider
+import com.example.gmakers_android.data.local.SharedPreferenceStorage
 import com.example.gmakers_android.data.remote.sign.SignApi
 import com.example.gmakers_android.feature.sign.model.LoginRequest
 import com.example.gmakers_android.feature.sign.model.LoginResponse
@@ -10,42 +12,32 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class LoginViewModel():ViewModel() {
-    val loginInterface = ApiProvider.getClient().create(SignApi::class.java)
+class LoginViewModel() : ViewModel() {
+    val loginInterface = ApiProvider.getInstnace().create(SignApi::class.java)
 
     val userId = MutableLiveData<String>()
-    val idDone = MutableLiveData(false)
 
     val userPassword = MutableLiveData<String>()
-    val passwordDone = MutableLiveData(false)
 
-    private val goRegister = MutableLiveData(false)
+    private val _toastMessage = MutableLiveData<String>()
+    val toastMessage: LiveData<String> get() = _toastMessage
+
 
     fun doLogin() {
-        if (idDone.value!! && passwordDone.value!!) {
-            val loginCall = loginInterface.doLogin(LoginRequest(userId.value!!,userPassword.value!!))
+            val loginCall =
+                loginInterface.doLogin(LoginRequest(userId.value!!, userPassword.value!!))
             loginCall.enqueue(object : Callback<LoginResponse> {
-                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
-                    when (response.code()) {
-                        200 -> {
-
-                        }
-                        else -> {
-
-                        }
+                override fun onResponse(
+                    call: Call<LoginResponse>,
+                    response: Response<LoginResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        _toastMessage.value = "로그인 성공"
                     }
                 }
                 override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                    Log.d("LoginActivity", t.toString())
+                    _toastMessage.value = "로그인 실패"
                 }
             })
-        }
     }
-    fun goRegister(){
-        userId.value = null
-        userPassword.value = null
-        goRegister.value = true
-    }
-
-
 }
