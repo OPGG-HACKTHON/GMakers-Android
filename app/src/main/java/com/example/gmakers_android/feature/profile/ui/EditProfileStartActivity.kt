59592bcf95153e1e799d6365f266058f77abe30e
dlswer23 <lbj4298@naver.com>
2021-08-27 23:53:39 +0900

    [UPDATE] all

--- app/src/main/java/com/example/gmakers_android/feature/profile/ui/EditProfileStartActivity.kt
+++ app/src/main/java/com/example/gmakers_android/feature/profile/ui/EditProfileStartActivity.kt
@@ -2,7 +2,6 @@ package com.example.gmakers_android.feature.profile.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.gmakers_android.R
 
 class EditProfileStartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
