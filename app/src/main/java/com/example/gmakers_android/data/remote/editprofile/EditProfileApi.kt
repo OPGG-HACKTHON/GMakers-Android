5ca9c6fd90519b070e7a546b9ff1978c2f9ffd3f
dlswer23 <lbj4298@naver.com>
2021-08-28 23:59:16 +0900

    [UPDATE] profile

---
+++ app/src/main/java/com/example/gmakers_android/data/remote/editprofile/EditProfileApi.kt
@@ -0,0 +1,4 @@
+package com.example.gmakers_android.data.remote.editprofile
+
+interface EditProfileApi {
+}
---
+++ app/src/main/java/com/example/gmakers_android/feature/profile/model/ChampionRequest.kt
@@ -0,0 +1,3 @@
+package com.example.gmakers_android.feature.profile.model
+
+data class ChampionRequest()
