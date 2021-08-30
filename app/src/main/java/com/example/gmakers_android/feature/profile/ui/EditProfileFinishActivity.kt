cab1afa298e03d030d42683b4a8a795ec72a26e3
dlswer23 <lbj4298@naver.com>
2021-08-30 23:27:01 +0900

    [STYLE] finish edit card

---
+++ app/src/main/java/com/example/gmakers_android/feature/profile/ui/EditProfileFinishActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gmakers_android.feature.profile.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProfileFinishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile_finish)
+    }
+}
